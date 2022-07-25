029239af55bc8625c05af41e2f7b39e63c482ad0
Orlando Jiménez <orlandoj922@gmail.com>
2022-07-24 20:01:07 -0600

    Add: Agregando paginador en la tabla
    Agregando un fragment para el paginador en la vista "Listar"

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -3,17 +3,23 @@ package com.bolsadeideas.springboot.app.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
+import com.bolsadeideas.springboot.app.util.paginator.PageRender;
 
 @Controller
 @SessionAttributes("cliente")
@@ -23,9 +29,15 @@ public class ClienteController {
 	private IClienteService clienteService;
 	
 	@GetMapping("/listar")
-	public String listar(Model model) {
+	public String listar(@RequestParam(name="page", defaultValue="0") int page, Model model) {
+		Pageable pageRequest = PageRequest.of(page, 5);
+		
+		Page<Cliente> clientes = clienteService.listarTodos(pageRequest);
+		
+		PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes", clienteService.listarTodos());
+		model.addAttribute("clientes", clientes);
+		model.addAttribute("page", pageRender);
 		return "listar";
 	}
 	
@@ -39,13 +51,18 @@ public class ClienteController {
 	}
 	
 	@GetMapping("/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Model model) {
+	public String editar(@PathVariable(value="id") Long id, Model model, RedirectAttributes flash) {
 		
 		Cliente cliente = null;
 		
 		if(id > 0) {
 			cliente = clienteService.buscarPorId(id);
+			if(cliente == null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la base de datos");
+				return "redirect:/listar";
+			}
 		} else {
+			flash.addFlashAttribute("error", "El ID del cliente no puede ser cero!");
 			return "redirect:/listar";
 		}
 		model.addAttribute("titulo", "Editar datos del cliente");
@@ -55,7 +72,7 @@ public class ClienteController {
 	}
 	
 	@PostMapping("/form")
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
 		if(result.hasErrors()) {
 			if(cliente.getId() != null && cliente.getId() > 0) {
 				model.addAttribute("titulo", "Editar datos del cliente");
@@ -67,16 +84,19 @@ public class ClienteController {
 				return "form";
 			}
 		}
+		String mensajeFlash = (cliente.getId() != null)? "Cliente editado con éxito!":"Cliente creado con éxito";
 		
 		clienteService.guardar(cliente);
 		status.setComplete();
+		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:listar";
 	}
 	
 	@GetMapping("/eliminar/{id}")
-	public String eliminar(@PathVariable(value="id") Long id, Model model) {
+	public String eliminar(@PathVariable(value="id") Long id, RedirectAttributes flash, Model model) {
 		if (id > 0) {
 			clienteService.eliminar(id);
+			flash.addFlashAttribute("success", "Cliente eliminado con éxito.");
 		}
 		return "redirect:/listar";
 	}
--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
@@ -2,15 +2,20 @@ package com.bolsadeideas.springboot.app.models.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 public interface IClienteService {
 
-public List<Cliente> listarTodos();
+	public List<Cliente> listarTodos();
 	
 	public void guardar(Cliente cliente);
 	
 	public Cliente buscarPorId(Long id);
 	
 	public void eliminar(Long id);
+	
+	public Page<Cliente> listarTodos(Pageable pageable);
 }
